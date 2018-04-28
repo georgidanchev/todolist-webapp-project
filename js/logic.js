fcd44bd2c104fa3febceacb53db9f408dfcbfff7
Dr.PenutButter <danchev49@protonmail.com>
2018-04-28 05:23:33 +0100

    table buttons implemented, YAY!!!

--- js/logic.js
+++ js/logic.js
@@ -1,48 +1,72 @@
 var htmlString = "";
 var htmlWrapper = document.querySelector('body');
 var inputSearchBox = document.querySelector('#searchBox');
+var controlButtonsVar;
 var hasSearched = false;
 
 var todoListArray = [{
-  tName: 'Cook some food',
-  dateOfCom: '03/05/18',
-  tPriority: 1,
-  controls: "/"
-}, {
-  tName: 'Take out the trash',
-  dateOfCom: '03/05/18',
-  tPriority: 2,
-  controls: "/"
-},
-{
-  tName: 'Go out for a jog',
-  dateOfCom: '03/05/18',
-  tPriority: 1,
-  controls: "/"
-},
-{
-  tName: 'Clean the house',
-  dateOfCom: '03/05/18',
-  tPriority: 1,
-  controls: "/"
-}];
+    tName: 'Cook some food',
+    dateOfCom: '03/05/18',
+    tPriority: 1
+  }, {
+    tName: 'Take out the trash',
+    dateOfCom: '03/05/18',
+    tPriority: 2
+  },
+  {
+    tName: 'Go out for a jog',
+    dateOfCom: '03/05/18',
+    tPriority: 1
+  },
+  {
+    tName: 'Clean the house',
+    dateOfCom: '03/05/18',
+    tPriority: 1
+  }
+];
 
 // run this code only when script first loads.
 window.onload = function() {
   webPrint();
 };
 
+
+function addTableButtons() {
+  controlButtonsVar = document.querySelectorAll('.controlButtons');
+
+  for (let j = 0; j < controlButtonsVar.length; j++) {
+    let up = document.createElement('button');
+    up.className = 'up';
+    up.textContent = "up";
+    controlButtonsVar[j].appendChild(up);
+
+    let down = document.createElement('button');
+    down.className = 'dn';
+    down.textContent = "dn";
+    controlButtonsVar[j].appendChild(down);
+
+    let remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.textContent = 'x';
+    controlButtonsVar[j].appendChild(remove);
+  }
+}
+
 function webPrint() {
   htmlString = '';
   htmlString += '<thead><tr><th scope="col">Task Name</th><th scope="col">Priority</th><th scope="col">Completion Date</th><th scope="col">Controls</th></tr></thead><tbody>';
 
   for (var i = 0; i < todoListArray.length; i++) {
-      htmlString += '<tr><th scope="row">' + todoListArray[i].tName + '</th><td>' + todoListArray[i].tPriority + '</td><td>' + todoListArray[i].dateOfCom + '</td><td>' + todoListArray[i].controls+ '</td></tr>';
+    htmlString += '<tr><th scope="row">' +
+      todoListArray[i].tName + '</th><td>' +
+      todoListArray[i].tPriority + '</td><td>' +
+      todoListArray[i].dateOfCom +
+      '</td><td class = "controlButtons"></td></tr>';
   }
 
   htmlString += '</tbody>';
   document.querySelector('#todoHTML').innerHTML = htmlString;
-  console.log(htmlString);
+  addTableButtons();
 }
 
 htmlWrapper.addEventListener('click', (event) => {
@@ -52,7 +76,7 @@ htmlWrapper.addEventListener('click', (event) => {
 });
 
 while (hasSearched === true) {
-  if(inputSearchBox.text == todoListArray.Name) {
+  if (inputSearchBox.text == todoListArray.Name) {
 
   }
 }
