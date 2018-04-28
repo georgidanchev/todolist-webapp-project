ea8ff07cbaec9f013aae90dafa5f0f3504af2c1e
Dr.PenutButter <danchev49@protonmail.com>
2018-04-28 04:24:34 +0100

    The calm before the storm

--- js/logic.js
+++ js/logic.js
@@ -1,25 +1,30 @@
 var htmlString = "";
 var htmlWrapper = document.querySelector('body');
-var inputSearchBox = document.querySelector('.searchBox');
+var inputSearchBox = document.querySelector('#searchBox');
+var hasSearched = false;
 
 var todoListArray = [{
   tName: 'Cook some food',
   dateOfCom: '03/05/18',
-  tPriority: 1
+  tPriority: 1,
+  controls: "/"
 }, {
   tName: 'Take out the trash',
   dateOfCom: '03/05/18',
-  tPriority: 2
+  tPriority: 2,
+  controls: "/"
 },
 {
   tName: 'Go out for a jog',
   dateOfCom: '03/05/18',
-  tPriority: 1
+  tPriority: 1,
+  controls: "/"
 },
 {
   tName: 'Clean the house',
   dateOfCom: '03/05/18',
-  tPriority: 1
+  tPriority: 1,
+  controls: "/"
 }];
 
 // run this code only when script first loads.
@@ -29,19 +34,15 @@ window.onload = function() {
 
 function webPrint() {
   htmlString = '';
-  htmlString += '<thead><tr><th scope="col">Task Name</th><th scope="col">Priority</th><th scope="col">Completion Date</th></tr></thead><tbody>';
-  for (var a = 0; a < todoListArray.length; a++) {
-    htmlString += '<tr>';
-    specificPrint(a);
-    htmlString += '</tr>';
+  htmlString += '<thead><tr><th scope="col">Task Name</th><th scope="col">Priority</th><th scope="col">Completion Date</th><th scope="col">Controls</th></tr></thead><tbody>';
+
+  for (var i = 0; i < todoListArray.length; i++) {
+      htmlString += '<tr><th scope="row">' + todoListArray[i].tName + '</th><td>' + todoListArray[i].tPriority + '</td><td>' + todoListArray[i].dateOfCom + '</td><td>' + todoListArray[i].controls+ '</td></tr>';
   }
+
   htmlString += '</tbody>';
   document.querySelector('#todoHTML').innerHTML = htmlString;
   console.log(htmlString);
-}
-
-function specificPrint(i) {
-  htmlString += '<th scope="row">' + todoListArray[i].tName + '</th><td>' + todoListArray[i].tPriority + '</td><td>' + todoListArray[i].dateOfCom + '</td>';
 }
 
 htmlWrapper.addEventListener('click', (event) => {
@@ -49,3 +50,9 @@ htmlWrapper.addEventListener('click', (event) => {
     if (event.target.id == "addBtn") {}
   }
 });
+
+while (hasSearched === true) {
+  if(inputSearchBox.text == todoListArray.Name) {
+
+  }
+}
