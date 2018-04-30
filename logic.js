5507fc89d8c74c9d9ea6cefd466236b74c10bd54
Dr.PenutButter <danchev49@protonmail.com>
2018-04-30 01:01:50 +0100

    search function done!

--- logic.js
+++ logic.js
@@ -1,8 +1,12 @@
 var htmlWrapper = document.querySelector('body'),
-inputBox = document.querySelector('#inputBox');
-var htmlString = "";
-var controlButtonsVar;
-var hasSearched = false;
+  inputBox = document.querySelector('#inputBox'),
+  searchNodeColor = 'red',
+  colorChangedNode,
+  defultNodeColor,
+  hasSearched = false,
+  htmlString,
+  controlButtonsVar,
+  length;
 
 var todoListArray = [{
     tName: 'Cook some food',
@@ -30,105 +34,73 @@ function msg(str) {
   console.log(str);
 }
 
+function resetInput() {
+  inputBox.value = "";
+}
+
 // run this code only when script first loads.
 window.onload = function() {
   webPrint();
 };
 
-
-function addTableButtons() {
+function addTableBtns(j) {
   controlButtonsVar = document.querySelectorAll('.controlButtons');
-  var length = controlButtonsVar.length;
+  length = controlButtonsVar.length;
 
-  for (let a = 0; a < length; a++) {
-    controlButtonsVar[a].innerHTML = "";
+  for (let x = 0; x < length; x++) {
+    controlButtonsVar[x].innerHTML = "";
   }
 
   for (let j = 0; j < length; j++) {
-    if (j != 0 && j > 0) {
-      let up = document.createElement('button');
-      up.className = 'up';
-      up.textContent = 'up';
-      controlButtonsVar[j].appendChild(up);
+    if (j !== 0) {
+      addUpButton(j, true);
     } else {
-      let up = document.createElement('button');
-      up.style.visibility = 'hidden';
-      up.textContent = 'up';
-      controlButtonsVar[j].appendChild(up);
+      addUpButton(j, false);
     }
 
-    if (j != length - 1) {
-      let down = document.createElement('button');
-      down.className = 'dn';
-      down.textContent = "dn";
-      controlButtonsVar[j].appendChild(down);
+    if (j !== length - 1) {
+      addDnButton(j, true);
     } else {
-      let down = document.createElement('button');
-      down.style.visibility = 'hidden';
-      down.textContent = "dn";
-      controlButtonsVar[j].appendChild(down);
+      addDnButton(j, false);
     }
-
-    let remove = document.createElement('button');
-    remove.className = 'remove';
-    remove.textContent = 'x';
-    controlButtonsVar[j].appendChild(remove);
+    addeXbutton(j);
   }
 }
 
-function webPrint() {
-  htmlString = '';
-  htmlString += '<thead><tr><th scope="col">Task Name</th><th scope="col">Priority</th><th scope="col">Completion Date</th><th scope="col">Controls</th></tr></thead><tbody>';
+function addUpButton(j, visibility) {
+  let up = document.createElement('button');
+  up.textContent = 'up';
 
-  for (var i = 0; i < todoListArray.length; i++) {
-    htmlString += '<tr><th scope="row">' +
-      todoListArray[i].tName + '</th><td>' +
-      todoListArray[i].tPriority + '</td><td>' +
-      todoListArray[i].dateOfCom +
-      '</td><td class = "controlButtons"></td></tr>';
+  if (visibility == true) {
+    up.className = 'up';
+  } else {
+    up.style.visibility = 'hidden';
   }
-
-  htmlString += '</tbody>';
-  document.querySelector('#todoHTML').innerHTML = htmlString;
-  addTableButtons();
+  controlButtonsVar[j].appendChild(up);
 }
 
-htmlWrapper.addEventListener('click', (event) => {
-  if (event.target.tagName == 'BUTTON') {
-    if (event.target.className == 'remove') {
-      let btn = event.target.parentNode;
-      let td = btn.parentNode;
-      let tr = td.parentNode;
-      tr.removeChild(td);
-    }
-    if (event.target.className == 'up') {
-      let btn = event.target.parentNode;
-      let tr = btn.parentNode;
-      let prevTr = tr.previousElementSibling;
-      let tbody = tr.parentNode;
-      tbody.insertBefore(tr, prevTr);
-    }
-    if (event.target.className == 'dn') {
-      let btn = event.target.parentNode;
-      let tr = btn.parentNode;
-      let nextTr = tr.nextElementSibling;
-      let tbody = tr.parentNode;
-      tbody.insertBefore(nextTr, tr);
-    }
-    if (event.target.id == "addBtn" && inputBox.value != 0) {
-      createNewTableRow();
-    }
-    if (event.target.id == "srchBtn") {
+function addDnButton(j, visibility) {
+  let down = document.createElement('button');
+  down.textContent = "dwn";
 
-
-    }
-    addTableButtons();
+  if (visibility == true) {
+    down.className = 'down';
+  } else {
+    down.style.visibility = 'hidden';
   }
-});
+  controlButtonsVar[j].appendChild(down);
+}
 
-function createNewTableRow(input) {
+function addeXbutton(j, visibility) {
+  let remove = document.createElement('button');
+  remove.className = 'remove';
+  remove.textContent = 'x';
+  controlButtonsVar[j].appendChild(remove);
+}
+
+function createTableRow(input) {
   var msg1 = 'Please enter task Priority - e.g. (1,2,3).',
-  msg2 = 'Please enter date of completion. i.e dd/mm/yy';
+    msg2 = 'Please enter date of completion. i.e dd/mm/yy';
 
   var tr = document.createElement('tr'),
     th = document.createElement('th'),
@@ -147,10 +119,85 @@ function createNewTableRow(input) {
   td1Pri.textContent = prompt(msg1);
   td2Dat.textContent = prompt(msg2);
   document.querySelectorAll('tbody')[0].appendChild(tr);
+  resetInput();
+  addTableBtns();
 }
 
-while (hasSearched === true) {
-  if (inputBox.text == todoListArray.Name) {
+function webPrint() {
+  htmlString = '';
+  htmlString += '<thead><tr><th scope="col">Task Name</th><th scope="col">Priority</th><th scope="col">Completion Date</th><th scope="col">Controls</th></tr></thead><tbody>';
 
+  for (var i = 0; i < todoListArray.length; i++) {
+    htmlString += '<tr><th scope="row" class="tName">' +
+      todoListArray[i].tName + '</th><td>' +
+      todoListArray[i].tPriority + '</td><td>' +
+      todoListArray[i].dateOfCom +
+      '</td><td class = "controlButtons"></td></tr>';
   }
+
+  htmlString += '</tbody>';
+  document.querySelector('#todoHTML').innerHTML = htmlString;
+  addTableBtns();
 }
+
+function tableBtnRespond(btnClass, eventTarget) {
+  let td = eventTarget.target.parentNode;
+  let tr = td.parentNode;
+  let tbody = tr.parentNode;
+
+  if (btnClass == 'remove') {
+    tbody.removeChild(tr);
+  } else if (btnClass == 'up') {
+    let prevTr = tr.previousElementSibling;
+    tbody.insertBefore(tr, prevTr);
+  } else if (btnClass == 'down') {
+    let nextTr = tr.nextElementSibling;
+    tbody.insertBefore(nextTr, tr);
+  }
+  addTableBtns();
+}
+
+function findWord(word, str) {
+  return RegExp('\\b' + word + '\\b').test(str);
+}
+
+function searchObjArray() {
+  hasSearched = false;
+  var tableNames = document.querySelectorAll('.tName');
+  if (colorChangedNode) {
+    colorChangedNode.style.background = defultNodeColor;
+  }
+
+  if (inputBox.value != 0) {
+    var uSearch = inputBox.value.toLowerCase();
+    for (var i = 0; i < tableNames.length; i++) {
+      if (tableNames[i].textContent.toLowerCase().match(uSearch)) {
+        if (findWord(uSearch, tableNames[i].textContent.toLowerCase())) {
+          var targetNode = tableNames[i].parentNode;
+          defultNodeColor = targetNode.style.background;
+          colorChangedNode = targetNode;
+          colorChangedNode.style.background = searchNodeColor;
+          hasSearched = true;
+        }
+      }
+    }
+  }
+  resetInput();
+}
+
+htmlWrapper.addEventListener('click', (event) => {
+  if (event.target.tagName == 'BUTTON') {
+    var bName = event.target.className;
+    if (event.target.parentNode.className == "controlButtons") {
+      tableBtnRespond(bName, event);
+    }
+    if (inputBox.value != 0 || hasSearched == true) {
+      if (event.target.id == "addBtn") {
+        createTableRow();
+      }
+      if (event.target.id == "srchBtn") {
+        searchObjArray();
+      }
+    }
+  }
+});
